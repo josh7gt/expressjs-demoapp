04117779cca252b7afdf937856450967dbf98801
Josh <joshruben08@gmail.com>
2025-02-01 18:43:45 -0500

    Retrieve mock email data through a parced id.
    If the id is not successfully parced as an int, return 400. If the user was not found, return 404.

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,34 @@ import express from "express";
 
 const app = express();
 const PORT = process.env.PORT || 2999;
+const mockUsers = [
+    { id: 1, username: "John", display: "John Doe" },
+    { id: 2, username: "Jane", display: "Jane Doe"},
+    { id: 3, username: "Jill", display: "Jill Doe"}
+];
 
 app.get("/", (req, res) => {
   res.status(201).send({msg: "Hello"});
 });
 
 app.get("/api/users", (req, res) => {
-  res.send([
-  { id: 1, username: "John", display: "John Doe" },
-  { id: 2, username: "Jane", display: "Jane Doe"},
-  { id: 3, username: "Jill", display: "Jill Doe"}
-  ]);
+  res.send(mockUsers);
 });
 
-
+app.get("/api/users/:id", (req, res) => {
+    console.log(req.params.id);
+    const parsedID = parseInt(req.params.id);
+    if (isNaN(parsedID)) {
+        res.status(400).send({msg: "Invalid ID"});
+    } else {
+        const user = mockUsers.find(user => user.id === parsedID);
+        if (user) {
+            res.send(user);
+        } else {
+            res.status(404).send({msg: "User not found"});
+        }
+    }
+})
 
 app.get('/api/products', (req, res) => {
   res.send([
