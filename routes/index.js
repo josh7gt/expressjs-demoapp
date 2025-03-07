cf98e0e322630d7b9e908ebcdd58ce654462238c
Josh <joshruben08@gmail.com>
2025-03-07 16:13:15 -0500

    Query filter validators configured - Make sure the query is a string - Make sure the query filter is not empty - Make sure the query filter length is between 3 and 10 - Console log the request errors

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 import express from "express";
+import {query, validationResult} from "express-validator";
 
 // initialize express
 const app = express();
 app.use(express.json());
-
 
 // a middleware function that logs the request method and url
 const loggingMiddleware = (req, res, next) => {
@@ -14,7 +14,7 @@ const loggingMiddleware = (req, res, next) => {
 // a middleware function that resolves findUserIndex based on the id
 const resolveIndexByUserID = (req, res, next) => {
     const {
-        params: { id },
+        params: {id},
     } = req;
 
     // return an error if the id is not a number
@@ -42,9 +42,9 @@ const PORT = process.env.PORT || 2999;
  * contains fake data types such as id, username, and display name
  */
 const mockUsers = [
-    { id: 1, username: "John", display: "John Doe" },
-    { id: 2, username: "Jane", display: "Jane Doe"},
-    { id: 3, username: "Jill", display: "Jill Doe"},
+    {id: 1, username: "John", display: "John Doe"},
+    {id: 2, username: "Jane", display: "Jane Doe"},
+    {id: 3, username: "Jill", display: "Jill Doe"},
 ];
 
 // create a route for the root of the server
@@ -53,12 +53,17 @@ app.get("/", (req, res) => {
 });
 
 // get requests in order to grab all user data through filters
-app.get("/api/users", (req, res) => {
-    console.log(req.query);
+app.get("/api/users", query("filter")
+    .isString()
+    .notEmpty()
+    .isLength({min: 3, max: 10})
+    .withMessage("Must be 3-10 characters"), (req, res) => {
+    const result = validationResult(req);
+    console.log(result);
 
     // destructure the query object from the request with filter and value parameters
     const {
-        query: { filter, value },
+        query: {filter, value},
     } = req;
 
     // if there is no filter or value, return all users, otherwise return filtered data
@@ -73,17 +78,17 @@ app.get("/api/users", (req, res) => {
 
 // post requests in order to add new user data
 app.post("/api/users", (req, res) => {
-    const { body } = req;
+    const {body} = req;
 
     // essentially add a new user to the mockUsers array with a consecutive id and the user body
-    const newUser = { id: mockUsers[mockUsers.length - 1].id + 1, ...body };
+    const newUser = {id: mockUsers[mockUsers.length - 1].id + 1, ...body};
     mockUsers.push(newUser);
     return res.status(201).send(newUser);
 })
 
 // a get request to grab user data based on the id
 app.get("/api/users/:id", resolveIndexByUserID, (req, res) => {
-    const { findUserIndex } = req;
+    const {findUserIndex} = req;
     const user = mockUsers[findUserIndex];
     if (user) {
         res.send(user);
@@ -96,33 +101,33 @@ app.get("/api/users/:id", resolveIndexByUserID, (req, res) => {
 // a get request that returns a list of mock products
 app.get('/api/products', (req, res) => {
     res.send([
-        { id: 11, name: 'Apple', price: 1.99 },
-        { id: 12, name: 'Orange', price: 2.99 },
-        { id: 13, name: 'Banana', price: 0.99 }
+        {id: 11, name: 'Apple', price: 1.99},
+        {id: 12, name: 'Orange', price: 2.99},
+        {id: 13, name: 'Banana', price: 0.99}
     ]);
 });
 
 // a put request to update user data based on the id
 app.put('/api/users/:id', resolveIndexByUserID, (req, res) => {
-    const { body, findUserIndex } = req;
+    const {body, findUserIndex} = req;
 
     // update the user using the parsed id and the body
-    mockUsers[findUserIndex] = { id: mockUsers[findUserIndex].id, ...body };
+    mockUsers[findUserIndex] = {id: mockUsers[findUserIndex].id, ...body};
     return res.sendStatus(200);
 });
 
 // a patch request to update specific data based on the id
 app.patch('/api/users/:id', resolveIndexByUserID, (req, res) => {
-    const { body, findUserIndex } = req;
+    const {body, findUserIndex} = req;
 
     // once again update the user using the parsed id and the body
-    mockUsers[findUserIndex] = { ...mockUsers[findUserIndex], ...body };
+    mockUsers[findUserIndex] = {...mockUsers[findUserIndex], ...body};
     return res.sendStatus(200);
 });
 
 // a delete request that deletes a user based on the id
 app.delete("/api/users/:id", resolveIndexByUserID, (req, res) => {
-    const { findUserIndex } = req.params;
+    const {findUserIndex} = req.params;
 
     // a splice method used to delete the user based on the index, "1" param is necessary to specify one account
     mockUsers.splice(findUserIndex, 1);
